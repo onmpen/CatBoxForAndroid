aa2b0cd10774964f23e5a22677d6723107999033
xchacha20-poly1305 <139959885+xchacha20-poly1305@users.noreply.github.com>
2023-10-14 17:44:13 +0800

    build: update go

--- buildScript/init/action/go.sh
+++ buildScript/init/action/go.sh
@@ -5,8 +5,8 @@ source buildScript/init/env.sh
 mkdir -p $PWD/build/golang
 cd $golang
 
-curl -Lso go.tar.gz https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
-echo "b3075ae1ce5dab85f89bc7905d1632de23ca196bd8336afd93fa97434cfa55ae go.tar.gz" | sha256sum -c -
+curl -Lso go.tar.gz https://go.dev/dl/go1.21.3.linux-amd64.tar.gz
+echo "1241381b2843fae5a9707eec1f8fb2ef94d827990582c7c7c32f5bdfbfd420c8 go.tar.gz" | sha256sum -c -
 tar xzf go.tar.gz
 
 go version
