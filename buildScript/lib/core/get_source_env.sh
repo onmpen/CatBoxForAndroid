e77340658b17f57c1a7cdecf58d00565f6dd03c2
Misaka-blog <misakablog@protonmail.com>
2023-10-28 11:41:03 +0800

    1.2.8
    (cherry picked from commit 2fe829012c19c57039e436e40ee3816e5ec20714)

--- buildScript/lib/core/get_source_env.sh
+++ buildScript/lib/core/get_source_env.sh
@@ -1,8 +1,8 @@
 if [ ! -z $ENV_NB4A ]; then
-  export COMMIT_SING_BOX_EXTRA="9fa3e2ab4ffda7e8514a61e1dbb1d8d99077046b"
+	export COMMIT_SING_BOX_EXTRA="46f70738dc58be78ef9903dfb555e9989f687ce5"
 fi
 
 if [ ! -z $ENV_SING_BOX_EXTRA ]; then
-  source libs/get_source_env.sh
-  # export COMMIT_SING_BOX="91495e813068294aae506fdd769437c41dd8d3a3"
+	source libs/get_source_env.sh
+	# export COMMIT_SING_BOX="91495e813068294aae506fdd769437c41dd8d3a3"
 fi
